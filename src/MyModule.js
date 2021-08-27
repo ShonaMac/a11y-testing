acd6aefe89673aa11e2000c5da90adfe23117af9
ShonaMac <88375551+ShonaMac@users.noreply.github.com>
2021-08-27 15:54:44 +0100

    Update and rename app.js to MyModule.js

---
+++ src/MyModule.js
@@ -0,0 +1,11 @@
+var _MyModule = {};
+// public function
+_MyModule.Add = function (a, b) {
+    // your code here
+    return parseFloat(a) + parseFloat(b);
+};
+_MyModule.Subtract = function (a, b) {
+    // your code here
+    return parseFloat(a) - parseFloat(b);
+};
+module.exports = _MyModule;
