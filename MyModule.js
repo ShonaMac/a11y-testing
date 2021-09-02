0b1d25a6c7cc928db31e51a5082e29e3f6f64139
ShonaMac <88375551+ShonaMac@users.noreply.github.com>
2021-09-02 14:37:35 +0100

    Update MyModule.js

--- MyModule.js
+++ MyModule.js
@@ -8,9 +8,5 @@ _MyModule.Subtract = function (a, b) {
     // your code here
     return parseFloat(a) - parseFloat(b);
 };
-_MyModule.Multuply = function (a, b) {
-    // your code here
-    return parseFloat(a) * parseFloat(b);
-};
 
 module.exports = _MyModule;
