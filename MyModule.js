289c1b3dafb85f84fd2b0b9ab188e3e57e18e501
ShonaMac <88375551+ShonaMac@users.noreply.github.com>
2021-09-13 11:24:05 +0100

    Update MyModule.js

--- MyModule.js
+++ MyModule.js
@@ -9,4 +9,14 @@ _MyModule.Subtract = function (a, b) {
     return parseFloat(a) - parseFloat(b);
 };
 
+_MyModule.Multiply = function (a, b) {
+    // your code here
+    return parseFloat(a) * parseFloat(b);
+    
+};
+
+_MyModule.Division = function (a, b) {
+    // your code here
+    return parseFloat(a) / parseFloat(b);
+
 module.exports = _MyModule;
